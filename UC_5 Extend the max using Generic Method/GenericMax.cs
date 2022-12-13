fe4cdeaa748ff033b5029e3c3c87b54ba599ad80
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-13 20:05:20 +0530

    Extend max using generic method

---
+++ UC_5 Extend the max using Generic Method/GenericMax.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extend_the_max_using_Generic_Method
+{
+    public class GenericMax<T> where T : IComparable
+    {
+        public T[] value;
+        public GenericMax(T[] value)
+        {
+            this.value = value;
+        }
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[^1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum value is " + max);
+        }
+    }
+}
---
+++ UC_5 Extend the max using Generic Method/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Extend_the_max_using_Generic_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 105, 2159, 1125, 789, 998 };
+            GenericMax<int> obj = new GenericMax<int>(arr);
+            obj.PrintMaxValue();
+        }
+    }
+}
