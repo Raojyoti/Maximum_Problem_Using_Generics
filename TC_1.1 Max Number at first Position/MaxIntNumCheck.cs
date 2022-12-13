fe7b59f34c074993b43dd705c4f28fe3cc8ccadb
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-13 20:13:52 +0530

    Max value at first position

---
+++ TC_1.1 Max Number at first Position/MaxIntNumCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TC_1._1_Max_Number_at_first_Position
+{
+    public class MaxIntNumCheck
+    {
+        public static int MaxIntegerNumCheck(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstValue, secondValue, thirdValue are same");
+        }
+    }
+}
---
+++ TC_1.1 Max Number at first Position/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TC_1._1_Max_Number_at_first_Position
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int OutputInt = MaxIntNumCheck.MaxIntegerNumCheck(10, 50, 100);
+            Console.WriteLine("Maximum value in First position: {0} ", OutputInt);
+        }
+    }
+}
