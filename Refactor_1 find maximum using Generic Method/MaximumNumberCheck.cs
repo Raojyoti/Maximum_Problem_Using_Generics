5d8f6a58b8d97d6ee8b6bbf03a247c0589739d32
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-13 19:47:57 +0530

    find the maximum using Generic

---
+++ Refactor_1 find maximum using Generic Method/MaximumNumberCheck.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Refactor_1_find_maximum_using_Generic_Method
+{
+    public class MaximumNumberCheck<T> where T : IComparable
+    {
+        public T firstValue, secondValue, thirdValue;
+        public MaximumNumberCheck(T firstValue, T secondValue, T thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
+        public static T MaxIntegerNumCheck(T firstValue, T secondValue, T thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue; 
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstValue, secondValue, thirdValue are same");
+        }
+        public static T MaxFloatNumCheck(T firstValue, T secondValue, T thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstValue, secondValue, thirdValue are same");
+        }
+        public static T MaxStringNumCheck(T firstValue, T secondValue, T thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstValue, secondValue, thirdValue are same");
+        }
+        public T MaxMethod()
+        {
+            T max = MaximumNumberCheck<T>.MaxIntegerNumCheck(this.firstValue, this.secondValue, this.thirdValue);
+            return max;
+        }
+        public T MaxFloatMethod()
+        {
+            T max = MaximumNumberCheck<T>.MaxFloatNumCheck(this.firstValue, this.secondValue, this.thirdValue);
+            return max;
+        }
+        public T MaxStringMethod()
+        {
+            T max = MaximumNumberCheck<T>.MaxStringNumCheck(this.firstValue, this.secondValue, this.thirdValue);
+            return max;
+        }
+    } 
+}
---
+++ Refactor_1 find maximum using Generic Method/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Refactor_1_find_maximum_using_Generic_Method
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MaximumNumberCheck<int> obj = new MaximumNumberCheck<int>(10, 50, 30);
+            int result = obj.MaxMethod();
+            Console.WriteLine("Maximum integer value is: {0} ", result);
+            Console.WriteLine("--------------------------------------------------");
+            MaximumNumberCheck<float> obj1 = new MaximumNumberCheck<float>(100.25f, 20.65f, 30.358f);
+            float result1 = obj1.MaxFloatMethod();
+            Console.WriteLine("Maximum float value is: {0} ", result1);
+            Console.WriteLine("--------------------------------------------------");
+            MaximumNumberCheck<string> obj2 = new MaximumNumberCheck<string>("Orange", "Banana", "Peach");
+            string result2= obj2.MaxStringMethod();
+            Console.WriteLine("Maximum string value is: {0} ", result2);
+            Console.WriteLine("--------------------------------------------------");
+           
+        }
+    }
+}
