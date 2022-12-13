e3e83e33ee873a9d175c43fcdffa3bc29eddb675
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-13 19:40:28 +0530

    Given three string find maximum value

---
+++ UC_3 three Strings find Maximum/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UC_3_three_Strings_find_Maximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string OutputString = MaximumStringCheck.MaxStringValueCheck("Orange", "Peach", "Banana");
+            Console.WriteLine("Maximum string value is: {0}",OutputString);
+        }
+    }
+}
