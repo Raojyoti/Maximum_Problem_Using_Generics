6811ac85930cecaa950cea724e088f1f6e014786
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-13 19:35:40 +0530

    Given three float find maximum value

---
+++ UC_2 three Floats find Maxium/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UC_2_three_Floats_find_Maxium
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            float OutputFloat = MaximumNumberCheck.MaxfloatNumCheck(101.25f, 5.52f, 81.30f);
+            Console.WriteLine("Maximum float number is {0} ",OutputFloat);
+        }
+    }
+}
