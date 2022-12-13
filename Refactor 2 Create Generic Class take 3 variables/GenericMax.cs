ef596bb3f63a6c796e49a7e2abb9e3fe8dc5b48a
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-13 19:54:10 +0530

    Create Generic Class take 3 variables

---
+++ Refactor 2 Create Generic Class take 3 variables/GenericMax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactor_2_Create_Generic_Class_take_3_variables
+{
+    public class GenericMax<T> where T : IComparable
+    {
+        public T firstValue, secondValue, thirdValue;
+        public GenericMax(T firstValue, T secondValue, T thirdValue) 
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
+        public static T MaxIntValue(T firstValue, T secondValue, T thirdValue) 
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstValue, secondValue, thirdValue are same"); 
+        }
+        public T MaxMethod()
+        {
+           T max =GenericMax<T>.MaxIntValue(this.firstValue, this.secondValue, this.thirdValue);
+            return max;
+        }
+    }
+}
---
+++ Refactor 2 Create Generic Class take 3 variables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Refactor_2_Create_Generic_Class_take_3_variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericMax<int> obj = new GenericMax<int>(10, 50, 30);
+            int result=obj.MaxMethod();
+            Console.WriteLine("Maximum value is: {0} ",result);
+            Console.WriteLine("--------------------------------");
+        }
+    }
+}
