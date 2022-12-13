53315472a4da7aa376b169bfcd9614cf66394805
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-13 19:58:41 +0530

    Extend max using sorting

---
+++ UC_4  Extend the max using Sorting/GenericMax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace UC_4__Extend_the_max_using_Sorting
+{
+    public class GenericMax<T> where T : IComparable
+    {
+        public T[] value;
+        public GenericMax(T[] value) 
+        {
+            this.value = value;
+        }
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[^1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum value is " + max);
+        }
+    }
+}
---
+++ UC_4  Extend the max using Sorting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UC_4__Extend_the_max_using_Sorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 105, 2159, 1125, 789, 998 };
+            GenericMax<int> obj = new GenericMax<int>(arr);
+            obj.PrintMaxValue();
+            Console.WriteLine("-------------------------------");
+            float[] arr1 = { 105.25f, 25.9f, 350.29f, 78.925f, 998.8f };
+            GenericMax<float> obj1 = new GenericMax<float>(arr1);
+            obj1.PrintMaxValue();
+            Console.WriteLine("-------------------------------");
+            string[] arr2 = { "Mango", "Banana", "Orange", "Peach" };
+            GenericMax<string> obj2 = new GenericMax<string>(arr2);
+            obj.PrintMaxValue();
+            Console.WriteLine("-------------------------------");
+        }
+    }
+}
