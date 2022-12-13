aae8e49bb15951e3de32a4896a9d2ce86c1f8f9f
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-13 20:48:38 +0530

    Print array values using generic

---
+++ UC_7_Print_Array_using_Generics/Generic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC_7_Print_Array_using_Generics
+{
+    public class Generic
+    {
+        public static void ToPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("--------------------");
+        }
+       
+    }
+}
---
+++ UC_7_Print_Array_using_Generics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC_7_Print_Array_using_Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 5, 8, 10 };
+            double[] doubleArray = { 1.2, 2.5, 3.8, 5.25 };
+            char[] charArray = { 'A', 'B', 'T', 'K' };
+            Generic.ToPrint<int>(intArray);
+            Generic.ToPrint<double>(doubleArray);
+            Generic.ToPrint<char>(charArray);
+        }
+    }
+}
