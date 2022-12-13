4761e7422a02772499bdcd60db09c7cf779962e5
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-13 19:29:23 +0530

    Given three integers find maximum value

---
+++ UC-1 three Integers find Maxium/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UC_1_three_Integers_find_Maxium
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum values after checking condition are: ");
+            int OutputInt = MaximumNumberCheck.MaxIntegerNumCheck(10, 50, 100);
+            Console.WriteLine(OutputInt);
+        }
+    }
+}
