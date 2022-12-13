e9984dbd82e4b013ef5049bb35782ceb40f7bbda
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2022-12-13 20:45:31 +0530

    Print Array

---
+++ UC_6_PrintArray/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace UC_6_PrintArray
+{
+    public class Program
+    {
+        public static void ToPrint(int[] inputArray)
+        {
+            foreach(int element in inputArray)
+            {
+                Console.WriteLine(element); 
+            }
+            Console.WriteLine("--------------------");
+        }
+        public static void ToPrint(double[] inputArray)
+        {
+            foreach (double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("--------------------");
+        }
+        public static void ToPrint(char[] inputArray)
+        {
+            foreach (int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("--------------------");
+        }
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 5, 8, 10 };
+            double[] doubleArray = { 1.2, 2.5, 3.8, 5.25 };
+            char[] charArray = {'A', 'B', 'T', 'p' };
+            Program.ToPrint(intArray);
+            Program.ToPrint(doubleArray);
+            Program.ToPrint(charArray);
+
+        }
+    }
+}
